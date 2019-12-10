b770b720272289589b0afa9b68d145449d3db283
Annie <https://github.com/yujan1122/practice1.git>
2019-12-10 09:45:03 +0800

    手機內資料撈出(手機號碼,聯絡人....)

--- app/src/main/java/tw/org/iii/iiiand14/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiand14/MainActivity.java
@@ -1,14 +1,72 @@
 package tw.org.iii.iiiand14;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private ContentResolver contentResolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //https://developer.android.com/training/permissions/requesting
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_CALL_LOG)
+                        != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.READ_CALL_LOG},
+                    1);
+        } else {
+               init();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        }
+    }
+
+    private void init(){
+        contentResolver = getContentResolver();//get 內容解析者, 資料庫的資料實體
+    }
+
+    public void test1(View view) {
+        Cursor c =  contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,null,null,null);
+        //String[] fields = c.getColumnNames();
+        //for (String field:fields){
+        //    Log.v("brad", field);
+        //}
+        int indexName = c.getColumnIndex("display_name");;
+        int indexData1 = c.getColumnIndex("data1");
+
+        while(c.moveToNext()){
+            String name = c.getString(indexName);
+            String tel = c.getString(indexData1);
+            Log.v("brad",name+":"+tel);
+
+        }
+        c.close();
     }
 }
